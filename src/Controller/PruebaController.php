13c46b5bedaeb56f61fd09085458fcddebadd167
Jose Gomez <jose_smt_92@hotmail.com>
2020-02-08 21:53:40 +0100

    Creado login y fichero para estandarizar los tamaños de fuente de la aplicacion

--- src/Controller/PruebaController.php
+++ src/Controller/PruebaController.php
@@ -6,22 +6,39 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class PruebaController  extends AbstractController
+class PruebaController extends AbstractController
 {
 
-    /**
-     * @Route("/inicio", name="inicio")
-     */
-    public function inicio(){
-        return $this->render('principal.html.twig');
-    }
 
     /**
-     * @Route("/prueba/{n}", name="prueba")
+     * @Route("/login", name="login")
      */
-    public function prueba($n){
-        if($n > 7) return new Response('<html><body>Numero: '.$n.'</body></html>');
-        else return new Respone('<html><body>El numero debe ser mayor a 7</body></html>');
+    public function login(){
+        return $this->render('login.html.twig');
+    }
+
+
+    /**
+     * @Route("/logout", name="logout")
+     */
+    public function logOut(){
+        return $this->render('logout.html.twig');
+    }
+
+
+    /**
+     * @Route("/sign_in", name="signIn")
+     */
+    public function signIn(){
+        return $this->render('sign_in.html.twig');
+    }
+
+
+    /**
+     *  @Route("/inicio", name="inicio")
+     */
+    public function inicio(){
+        return $this->render('inicio.html.twig');
     }
     
 }
